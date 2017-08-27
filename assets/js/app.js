251cfb7418da037abd3e6e4838dedf71cc88a5cc
Laura Boetcher <g33klaura@gmail.com>
2017-08-27 15:27:25 -0400

    Psuedo code steps to complete

---
+++ assets/js/app.js
@@ -0,0 +1,38 @@
+// PSUEDO CODED STEPS TO ACCOMPLISH
+// API Key: e6e389c7f6fc432dba0ce6999e6c8123
+
+// Primary:
+// Page has array of buttons showing
+// Buttons, based off my pre-set array of titles, are created by jQuery
+// When each button is clicked, 
+// an Ajax call to the Giphy API is made for the following parameters: 
+	// 'q', 'rating', 'limit'****
+// 10 gifs render on the page, below the buttons
+// Each gif shows a rating above it
+// When gifs load, they're initially in a static state
+// Upon click, gifs will change to animated state
+// Clicking again returns to static
+// Secondary:
+// Input element with submit button allows user to add new buttons
+// Each new button is dynamically rendered with jQuery
+// Buttons have same functionality as primary buttons (return clickable gifs using new search term)
+
+
+
+
+// VARIABLES ====================
+// 
+
+
+
+
+
+// FUNCTIONS ====================
+// 
+
+
+
+
+
+// MAIN PROCESS ====================
+// 
